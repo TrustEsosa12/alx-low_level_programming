0ad5afdb3dd373398b79736b192e8ed799fc98ef
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-08 15:30:47 -0700

    a C program that prints exactly as asigned

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ 
+    * main - Entry point
+ 
+    *
+ 
+    * Return: 0 on success
+ 
+    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-a script that compiles a C file and creates an executable named cisfun
+a C program that prints exactly as assigned
