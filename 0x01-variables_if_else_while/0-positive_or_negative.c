d7e43635620656c8c8b0d00cbc226cfd761853c1
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-09 13:49:59 -0700

    the number is negative and positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,17 @@
 /**
  * main - Entry point
  * Return: Always 0
-iii*/
+ */
 int main(void)
 { 
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is positive\n",n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%d is nagative\n",n);
+printf("%d is negative\n", n);
 else
-printf("%d is zero\n",n);
+printf("%d is zero\n", n);
 return (0);
 }
