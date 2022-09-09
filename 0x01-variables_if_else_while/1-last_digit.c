4c8804fc68d14589271eba7e8028ca26f78ab305
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-09 16:01:20 -0700

    the last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,9 +14,10 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 printf("Last digit of %i is %i and is ", n, lastDigit);
 if (lastDigit > 5)
-printf("greater than S\n");
+printf("greater than 5\n");
 else if (lastDigit == 0)
 printf("0\n");
 else
-printf("less than 6 and 0\n");
+printf("less than 6 and not 0\n");
+
 return (0); }
