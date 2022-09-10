78a6aaebb6374eb57d2b9e30285f9a5de2e3a641
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-09 17:14:22 -0700

    the perfect commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{ char a = 'a';
+while (a <= 'z')
+{ putchar(a);
+a++;
+}
+a = 'A';
+while (a <= 'z')
+{ putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
