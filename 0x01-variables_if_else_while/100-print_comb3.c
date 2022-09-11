9d1dcf534d877a7802cec6d9887cd9ca0482d880
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-10 22:37:54 -0700

    the perfect commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,10 +6,10 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{ 
+{
 int c, i;
-for (c = '0'; c <= '9'; c++) 
-{ 
+for (c = '0'; c <= '9'; c++)
+{
 for (i = '0'; i <= '9'; i++)
 {
 if (c < i)
