588d414d20fe27d662606d5bbd73586821b56383
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-10 19:35:20 -0700

    a combination of brains and materials

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,12 +12,13 @@ for (c = '0'; c <= '9'; c++)
 { if (c < i)
 { putchar(c);
 putchar(i);
-if (c != '8' || (c == '8' && i != '9')
-{ putchar(';);
+if (c != '8' || (c == '8' && i != '9'))
+{ putchar(',');
 putchar(' ');
-} putchar 
-} }
-} } putchar('\n');
+}
+}
+}
+} putchar('\n');
 
 return (0);
 }
