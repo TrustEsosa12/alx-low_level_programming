427950e9eeeda86ef8647e7ec679a5a082e35781
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-11 00:46:46 -0700

    print alphabeth in lowercae then in uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,16 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{ 
-int ch; 
+{
+int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-{ 
+{
 putchar(ch);
 }
 for (ch = 'A'; ch <= 'Z'; ch++)
-{ 
+{
 putchar(ch);
-} 
+}
 putchar('\n');
 return (0);
 }
