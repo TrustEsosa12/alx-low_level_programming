6416069c71aa9059c752f930cb860d967daab779
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-13 18:20:30 -0700

    print the lower alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,16 +5,16 @@
  * Description: Write a function that prints the alphabet, in lowercase, followed by a new line
  * Return: Always 0 (sucess)
  */
-int main()
+int main();
 {
 int alphabet;
 alphabet = 'a';
 return("lowercase alphabet:\n");
 while(a <= 'z')
-{
+}
 putchar("%d/n", a);
 a++;
 }
 putchar('\n')
 return: 0;
-}
+{
