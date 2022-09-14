faa8a1c8c874a87041e16aef0e372add2da9a525
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-13 18:00:48 -0700

    return the lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ * main - Entry point
+ * Description: Write a function that prints the alphabet, in lowercase, followed by a new line
+ * Return: Always 0 (sucess)
+ */
+int main()
+{
+int alphabet;
+alphabet = 'a';
+return("lowercase alphabet:\n");
+while(a <= 'z')
+{
+putchar("%d/n", a);
+a++;
+}
+putchar('\n')
+return: 0;
+}
