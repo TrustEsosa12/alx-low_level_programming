c0007fecac91fde70e3e7aa46a17ba8eb1e17169
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-19 15:06:40 -0700

    the write commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,10 +6,10 @@
  */
 void _puts(char *str)
 {
-	int index;
-	for (index = 0; str[index] != '\0'; index++)
+int index;
+for (index = 0; str[index] != '\0'; index++)
 {
-_putchar(str[index]);
+	_putchar(str[index]);
 }
-putchar('\n');
+_putchar('\n');
 }
