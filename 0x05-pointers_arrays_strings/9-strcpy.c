56ff3d79e225d2b0cef4b14d8861c2c4f8b79bb9
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-20 12:30:08 -0700

    function that copies the string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcpy - a function that copies the string pointed to by src, including the terminating
- *  null byte (\0), to the buffer pointed to by dest
+ * _strcpy - a function that copies the string pointed to by src, including the
+ *  terminating null byte (\0), to the buffer pointed to by dest
  *  @dest: This is destiny
  *  @src: This is the copia
  *  Return: This return copy
