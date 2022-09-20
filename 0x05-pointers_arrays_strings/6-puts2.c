9da0571535feaec6425835ee7e5e24ce640272fc
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-20 05:30:31 -0700

    a function that prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * puts2 - Write a function that prints every other character of a string,
- * starting with the first character, followed by a new line 
+ * starting with the first character, followed by a new line
  * @s: this pointer point to a char or string
  */
 void puts2(char *s)
