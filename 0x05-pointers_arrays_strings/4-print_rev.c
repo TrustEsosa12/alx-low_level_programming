9d8c13dfbde14d2e093e3b652a4d0d0f0d7f5f95
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-20 01:06:54 -0700

    a function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  */
 void print_rev(char *s)
 {
-	int index;
-	for (index = 0; s[index] != '\0'; index++)
-		;
-	for (index = index - 1; s[index] != '\0'; index--)
-	{
-		putchar(s[index]);
-	}
-	putchar('\n');
+int index;
+for (index = 0; s[index] != '\0'; index++)
+;
+for (index = index - 1; s[index] != '\0'; index--)
+{
+putchar(s[index]);
+}
+putchar('\n');
 }
 
