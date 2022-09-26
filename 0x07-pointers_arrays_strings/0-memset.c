cc925e0a86241b715b5f0de891c6b0cc930be2b4
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-26 08:39:43 -0700

    function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _memset - fills the first n bytes of memory with a constant value
+ * _memset - fill n bytes of memory with a constant value
  * @s: pointer to memory area
  * @b: constant value
  * @n: number of bytes to fill
@@ -13,8 +13,8 @@ int size = n; /* only accept positive size */
 if (size > 0)
 {
 int i;
-for(i = 0; i < size; i++)
-s[i] = b; 
+for (i = 0; i < size; i++)
+s[i] = b;
 }
 return (s);
 }
