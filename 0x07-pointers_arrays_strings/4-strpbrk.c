099e049c17d87779ea16563578918cd1f798f337
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-26 23:04:55 -0700

    function locates the first occurrence in the string s

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strpbrk - return pointer to byte in s that matches a byte in accept
- * @s: string to search
- * @accept: target matches
- * Return: pointer to index of string at first occurance
+ *_strpbrk - return pointer to byte in s that matches a byte in accept
+ *@s: string to search
+ *@accept: target matches
+ *Return: pointer to index of string at first occurance
  */
 char *_strpbrk(char *s, char *accept)
 {
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-a function that prints the chessboard
+a function that searches a string for any of a set of bytes
