76cbd9cc6bf7a9d7b39c4ccfe85f7036ca1ef2ba
TrustEsosa12 <trustesoje123@gmail.com>
2022-09-28 20:39:41 -0700

    prints the sum of the two diagonals of a square matrix of integers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,4 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle;
 void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
-
+void set_string(char **s, char *to); 
