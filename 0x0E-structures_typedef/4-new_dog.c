f31abc8a280eb100eae1e75e313cf8ec552319a8
TrustEsosa12 <trustesoje123@gmail.com>
2022-10-10 13:23:57 -0700

    function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ i++;
 return (i);
 }
 /**
- *_strcpy - copies the string pointed to by src
+ * _strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
