ea1029c75f9283ea30f0035bcb094cf03dea1463
TrustEsosa12 <trustesoje123@gmail.com>
2022-10-12 07:27:25 -0700

    a program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -21,9 +21,7 @@ num1 = atoi(argv[1]);
 op = argv[2];
 num2 = atoi(argv[3]);
 if (get_op_func(op) == NULL || op[1] != '\0')
-{											
-printf("Error\n");									
-exit(99);
+{										printf("Error\n");								exit(99);
 }
 if ((*op == '/' && num2 == 0) ||
 (*op == '%' && num2 == 0))
