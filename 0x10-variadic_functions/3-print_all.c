a057180c1ec438c8c68e754e8100dc071ce0db4d
TrustEsosa12 <trustesoje123@gmail.com>
2022-10-13 09:32:24 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 /**
  * print_all - prints anything.
  * @format: a list of types of arguments passed to the function.
@@ -12,7 +14,8 @@ char *str;
 const char t_arg[] = "cifs";
 va_start(valist, format);
 while (format && format[i])
-}										j = 0;
+{
+j = 0;
 while (t_arg[j])
 {
 if (format[i] == t_arg[j] && c)
@@ -23,18 +26,25 @@ break;
 }
 switch (format[i])
 {
-case 'c':									printf("%c", va_arg(valist, int)), c = 1;
+case 'c':
+printf("%c", va_arg(valist, int)), c = 1;
 break;
 case 'i':
 printf("%d", va_arg(valist, int)), c = 1;
 break;
-case 'f':									printf("%f", va_arg(valist, double)), c = 1;					break;
-case 's'; 									str = va_arg(valist, char *), c = 1;
+case 'f':
+printf("%f", va_arg(valist, double)), c = 1;	
+break;
+case 's'; 
+str = va_arg(valist, char *), c = 1;
 if (!str)
 {
-printf("(nil)");								break;
-}										printf("%s", str);
-break;										} i++;
+printf("(nil)");
+break;
+}
+printf("%s", str);
+break;	
+} i++;
 }
 printf("\n"), va_end(valist);
 }
