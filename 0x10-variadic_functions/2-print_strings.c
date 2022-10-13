aff121745102ba8e3f4392d2ddea1e681b8f01c1
TrustEsosa12 <trustesoje123@gmail.com>
2022-10-13 08:10:01 -0700

    a function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ str = va_arg(strings, char *);
 if (str == NULL)
 printf("(nil)");
 else
-printf("%s", str);	
+printf("%s", str);
 if (index != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
