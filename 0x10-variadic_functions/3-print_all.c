72cb8c1b3ee2c27bb0a672953baab7334dbdda2b
TrustEsosa12 <trustesoje123@gmail.com>
2022-10-13 17:34:23 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,49 +2,44 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- * print_all - prints anything.
- * @format: a list of types of arguments passed to the function.
- * Return: no return.
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
  */
 void print_all(const char * const format, ...)
 {
-va_list valist;
-unsigned int i = 0, j, c = 0;
-char *str;
-const char t_arg[] = "cifs";
-va_start(valist, format);
-while (format && format[i])
+int i = 0;
+char *str, *sep = "";
+va_list list;
+va_start(list, format);
+if (format)
 {
-j = 0;
-while (t_arg[j])
+while (format[i])
 {
-if (format[i] == t_arg[j] && c)
-{
-printf(", ");
-break;
-} j++;
-}
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(valist, int)), c = 1;
+printf("%s%c", sep, va_arg(list, int));
 break;
 case 'i':
-printf("%d", va_arg(valist, int)), c = 1;
+printf("%s%d", sep, va_arg(list, int));
 break;
 case 'f':
-printf("%f", va_arg(valist, double)), c = 1;	
+printf("%s%f", sep, va_arg(list, double));
 break;
-case 's'; 
-str = va_arg(valist, char *), c = 1;
+case 's':
+str = va_arg(list, char *);
 if (!str)
-{
-printf("(nil)");
+str = "(nil)";
+printf("%s%s", sep, str);
 break;
+default:
+i++;
+continue;
 }
-printf("%s", str);
-break;	
-} i++;
+sep = ", ";
+i++;
 }
-printf("\n"), va_end(valist);
+}
+printf("\n");
+va_end(list);
 }
